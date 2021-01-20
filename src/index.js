1bc413326054e89391729ab944670644ad2b2e2f
nosleepfilipe <mail@filipedias.me>
2021-01-20 00:07:24 +0000

    refactor code to reflect new html on coinmarketcap, sync new icons

--- src/index.js
+++ src/index.js
@@ -1,71 +1,66 @@
-const downloadAndRetry = require('./downloadAndRetry')
-const getAndRetry = require('./getAndRetry')
-const cheerio = require('cheerio')
-const sleep = require('sleep-promise')
+const downloadAndRetry = require("./downloadAndRetry");
+const getAndRetry = require("./getAndRetry");
+const cheerio = require("cheerio");
+const sleep = require("sleep-promise");
 
-const HOMEPAGE = 'https://coinmarketcap.com'
+const HOMEPAGE = "https://coinmarketcap.com/";
+const COOL_DOWN = 2000;
 
 const parseCoinPage = async (page) => {
+  const url = HOMEPAGE + page;
 
-  const url = HOMEPAGE + page
+  const html = await getAndRetry(url);
 
-  const html = await getAndRetry(url)
+  await sleep(COOL_DOWN);
 
-  await sleep(3000)
+  const $ = cheerio.load(html);
 
-  const $ = cheerio.load(html)
+  const image = $('meta[property="og:image"]').attr("content");
 
-  const image = $('meta[property="og:image"]').attr('content')
+  const h2 = $("h2");
+  const symbol = h2.find("small").html();
 
-  const h1 = $('h1')
-  const symbol = h1
-    .find('span')
-    .eq(1)
-    .html()
-    .replace('(', '')
-    .replace(')', '')
+  console.log("downloading: ", symbol);
+  console.log("image", image);
 
-  console.log("downloading: ", symbol)
-
-  return downloadAndRetry(image, symbol)
-}
+  return downloadAndRetry(image, symbol);
+};
 
 const getPage = (number) => {
-  if(number === 1) {
-    return HOMEPAGE
+  if (number === 1) {
+    return HOMEPAGE;
   } else {
-    return HOMEPAGE + '/' + number + '/'
+    return HOMEPAGE + "/" + number + "/";
   }
-}
+};
 
 const parsePage = async () => {
+  for (let page = 1; page <= 10; page++) {
+    console.log("");
+    console.log(` -- parsing page ${page} --`);
+    console.log("");
 
-  for(let page = 1; page <= 11; page++) {
+    const html = await getAndRetry(getPage(page));
 
-    console.log("")
-    console.log(` -- parsing page ${page} --`)
-    console.log("")
+    const $ = cheerio.load(html);
 
-    const html = await getAndRetry(getPage(page))
+    const rows = $(".cmc-table tbody tr");
 
-    const $ = cheerio.load(html)
-  
-    const rows = $('.cmc-table-row')
-  
-    for(let index = 0; index < 100; index++) {
-      const row = rows.eq(index).find('a')
-  
-      const page = row.attr('href')
-  
-      await parseCoinPage(page)
-  
-      // we need to wait otherwise we are blocked with too many requests
-      await sleep(3000)
+    for (let index = 0; index < 100; index++) {
+      const row = rows.eq(index).children("td:nth-child(3)");
+
+      const page = row.children().attr("href");
+
+      console.log("INDEX ", index);
+      console.log("page found ", page);
+
+      if (page == undefined) continue;
+
+      await parseCoinPage(page);
+
+      await sleep(COOL_DOWN);
     }
-
   }
+};
 
-
-}
-
-parsePage()
+parsePage();
