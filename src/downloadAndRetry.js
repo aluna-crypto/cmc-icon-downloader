b856e768d1010f8867e81cfe1d15ea526a7489a9
Filipe Dias <mail@filipedias.me>
2021-09-27 11:42:50 +0100

    update CMC icons, add headers to simulate browser ( Cloudflare is blocking )

--- src/downloadAndRetry.js
+++ src/downloadAndRetry.js
@@ -1,31 +1,29 @@
-const sleep = require('sleep-promise')
-const {get} = require('request-promise')
-const fs = require('fs')
+const sleep = require("sleep-promise");
+const { get } = require("request-promise");
+const fs = require("fs");
 
-const COOL_DOWN = 10000
+const COOL_DOWN = 10000;
 
-module.exports = async function(uri, filename){
-  const path = "./images/" + filename.toLowerCase() + "@2x.png"
+module.exports = async function (uri, filename) {
+  const path = "./images/" + filename.toLowerCase() + "@2x.png";
 
-  while(true) {
+  while (true) {
     try {
       const response = await get({
         uri,
         encoding: null,
-        resolveWithFullResponse: true
-      })
-      
-      return fs.writeFileSync(path, response.body)
+        resolveWithFullResponse: true,
+      });
+
+      return fs.writeFileSync(path, response.body);
     } catch (error) {
-      if(error.statusCode === 429) {
-        console.log("too many requests, cooling down")
+      if (error.statusCode === 429) {
+        console.log("too many requests, cooling down");
       } else {
-        throw(error)
+        throw error;
       }
-      
-      await sleep(COOL_DOWN)
+
+      await sleep(COOL_DOWN);
     }
-    
   }
-  
 };
--- src/getAndRetry.js
+++ src/getAndRetry.js
@@ -1,20 +1,25 @@
-const sleep = require('sleep-promise')
-const {get} = require('request-promise')
+const sleep = require("sleep-promise");
+const { get } = require("request-promise");
 
-const COOL_DOWN = 10000
+const COOL_DOWN = 10000;
 
 module.exports = async (url) => {
-  while(true) {
+  while (true) {
     try {
-      return await get(url)
+      return await get(url, {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",
+        },
+      });
     } catch (error) {
-      if(error.statusCode === 429) {
-        console.log("too many requests, cooling down")
+      if (error.statusCode === 429) {
+        console.log("too many requests, cooling down");
       } else {
-        throw(error)
+        throw error;
       }
-      
-      await sleep(COOL_DOWN)
+
+      await sleep(COOL_DOWN);
     }
   }
-}
+};
